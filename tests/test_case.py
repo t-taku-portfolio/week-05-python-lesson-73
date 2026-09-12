320f2a76583d30226fb89a31cbfb0ee567514695
t-taku-portfolio <207686657+t-taku-portfolio@users.noreply.github.com>
2026-09-12 23:49:02 +0900

    test(normalcase): assert whether func return dict

--- tests/test_case.py
+++ tests/test_case.py
@@ -10,9 +10,17 @@ def test_failure_case1():
     with pytest.raises(ValueError):
         backup_utility.audit_directory_space("")
 
-def test_normal_case():
-    backup_utility.create_staged_backup(
+def test_normal_case1():
+    dst_dir = backup_utility.create_staged_backup(
         Path.cwd() / Path('src') / Path('sample_resource') / Path('sample_target'),
         Path.cwd(),
         'cmprsd_fldr'
     )
+    assert Path(dst_dir) == (Path.cwd() / 'cmprsd_fldr.zip')
+
+def test_normal_case2():
+    key_set = ["total", "used", "free"]
+    usage_dict = backup_utility.audit_directory_space(Path.home())
+    for key in usage_dict:
+        assert key in key_set
+        assert usage_dict[key] > 0
